7da4348d0eb03d3efa4a130e6529bdb41fd41fcf
jiansen <1096129293@qq.com>
2024-12-09 14:47:45 +0800

    fix:1

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
 	base: './',
-	build: {
-		outDir: 'docs' // 输出到项目根目录下的 dist 目录
-	},
 	plugins: [vue()]
 })
